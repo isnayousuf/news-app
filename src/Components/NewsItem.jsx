26f6936f864ac139244288ff929bea0fce1bad78
isna yousuf <isnayousuf77@gmail.com>
2025-03-15 13:12:46 +0530

    fix: add the bg image for news card

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,23 +1,33 @@
-import Image from "../assets/alt-news.svg";
+import Image from "../assets/news.webp";
 export const NewsItem = ({title, description, imageSrc, newsUrl}) => {
   return (
     <div
       className="card bg-dark text-light d-inline-block my-3 mx-3 px-2 py-2"
-      style={{ maxWidth: "345px" }}
+      style={{ maxWidth: "345px", height: "400px" }} // Fixed height for all cards
     >
       <div>Hello</div>
       <img
         src={imageSrc ? imageSrc : Image}
         className="card-img-top"
         alt="news-img"
-        style={{ height: "200px", width: "360px" }}
+        style={{ height: "200px", width: "100%", objectFit: "cover" }}
       />
-      <div className="card-body">
+      <div className="card-body d-flex flex-column">
         <h5 className="card-title">{title?.slice(0, 50)}</h5>
-        <p className="card-text">
-          {description ? description.slice(0, 90) : "News Description"}
+        <p
+          className="card-text"
+          style={{
+            flex: "1",
+            overflow: "hidden",
+            display: "-webkit-box",
+            WebkitLineClamp: 3, 
+            WebkitBoxOrient: "vertical",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {description ? description : "News Description"}
         </p>
-        <a href={newsUrl} className="btn btn-primary">
+        <a href={newsUrl} className="btn btn-primary mt-auto">
           Read More
         </a>
       </div>
